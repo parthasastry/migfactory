585e6956cb1488f97dd03b3b697447d3738bcf38
Sastry <pssastry@a483e77984df.ant.amazon.com>
2020-08-06 11:20:41 -0400

    added search functionality

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react'
-import { Container, Table } from 'semantic-ui-react'
+import { Search, Container, Table } from 'semantic-ui-react'
 import * as d3 from 'd3';
 import inventoryData from '../inventory.csv'
 
-const initialState = { isLoading: false, results: [], value: ''};
+const initialState = { inventory: [], results: []};
 
 export class Inventory extends Component {
     constructor(props){
         super(props);
         this.state = initialState;
-
+        this.handleSearchChange = this.handleSearchChange.bind(this)
     }
     
-
     componentDidMount() {
         d3.csv(inventoryData).then((data) => {
             console.log(data)
-            this.setState({results: data})
+            this.setState({inventory: data, results: data})
         })
     };
+
+    handleSearchChange(e){
+        console.log(e.target.value)
+        const text = e.target.value
+        if (text.length > 1) {
+            console.log(text)
+            const newResults = this.state.inventory.filter(function(d){
+                return d.Server_Name.includes(text) || d.App.includes(text)
+            })
+            this.setState({results: newResults})
+        } else {
+            this.setState({results: this.state.inventory})
+        }
+    }
 
     render() {
         const inventoryData = this.state.results.map((d, i) => {
@@ -30,8 +43,14 @@ export class Inventory extends Component {
                 <Table.Cell>{d['AD Migration Status']}</Table.Cell>
             </Table.Row>
         })
+
         return (
             <Container>
+                <Search 
+                    placeholder="Server or App"
+                    onSearchChange={this.handleSearchChange}
+                />
+
                 <Table striped>
                     <Table.Header>
                         <Table.Row>
